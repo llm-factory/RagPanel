49eadb3c94932ff72ea49419ff7ba58e211cd8cb
SUN Sun <sun.sun@utt.fr>
2024-07-08 17:48:20 +0800

    Add ui to launch cardinal

--- web_ui.py
+++ web_ui.py
@@ -1,12 +1,10 @@
 import gradio as gr
 from dotenv import load_dotenv
-
-
-load_dotenv()
-
-
 from src.engine import search, delete_by_id, replace, insert
 import pandas as pd
+from subprocess import Popen
+
+load_dotenv()
 
 
 def info_fn():
@@ -33,13 +31,28 @@ def delete_docs(ids: list, docs: pd.DataFrame):
     return docs
 
 
+def launch(filepath, action):
+    if filepath and action:
+        command = f"python ./api_demo/launch.py --config {filepath} --action {action}"
+        Popen(command, shell=True)
+
+        gr.Info("Success")
+
+
 with gr.Blocks() as demo:
     result_state = gr.State()
 
-    dropdown = gr.Dropdown(
-        choices=[1],
-        label="Select database"
-    )
+    with gr.Tab("Launch"):
+        with gr.Row(equal_height=False):
+            config_file = gr.File(
+                file_count="single",
+                file_types=[".yml", "yaml"],
+                label="Config file"
+            )
+
+            action = gr.Radio(["build", "launch", "dump"], label="Parameter", info="action")
+        launch_btn = gr.Button("Launch")
+
     with gr.Tab("Insert"):
         file = gr.File(
             file_count="single",
@@ -85,5 +98,7 @@ with gr.Blocks() as demo:
     search_btn.click(search_docs, [search_box, slider], result_state)
 
     replace_btn.click(replace, [replace_content, file], None)
+
+    launch_btn.click(launch, [config_file, action], None)
 
 demo.launch()
