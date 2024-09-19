c20950f4498a6e18351504ef36552995276b9a98
ILSparkle <1498283309@qq.com>
2024-09-19 02:47:43 -0700

    basic rag chat

--- src/RagPanel/chat_engine.py
+++ src/RagPanel/chat_engine.py
@@ -8,7 +8,6 @@ from .protocol import History
 
 class ChatEngine:
     def __init__(self, database: str, engine) -> None:
-        self._window_size = 6
         self._chat_model = ChatOpenAI()
         self._collector = BaseCollector[History](storage_name=database)
         self._kbqa_template = Template("充分理解以下事实描述：{context}\n\n回答下面的问题：{query}")
@@ -16,22 +15,24 @@ class ChatEngine:
         
     def get_history(self):
         histories = self._collector.dump()
+        ret = [[None, "您好，有什么我可以帮助您的吗？"]]
         if not len(histories):
-            return None
-        ret = []
+            return ret
         for history in histories:
             messages = [message.content for message in history.messages]
             ret.append(messages)
         return ret
     
-    def stream_chat(self, query, **kwargs) -> Generator[str, None, None]:
-        documents = self.engine.search(query, threshold=1, top_k=2)["content"].tolist()
-        if len(documents):
+    def stream_chat(self, history, query, threshold, top_k, **kwargs) -> Generator[str, None, None]:
+        search_result = self.engine.search(query, threshold=threshold, top_k=top_k)
+        if any(search_result):
+            documents = search_result["content"].tolist()
             query = self._kbqa_template.apply(context="\n".join(documents), query=query)
 
         augmented_messages = [HumanMessage(content=query)]
-        response = ""
+        history += [[query, None]]
+        history[-1][1] = ""
         for new_token in self._chat_model.stream_chat(augmented_messages, **kwargs):
-            yield new_token
-            response += new_token
-        self._collector.collect(History(messages=(augmented_messages + [AssistantMessage(content=response)])))
+            history[-1][1] += new_token
+            yield history
+        self._collector.collect(History(messages=(augmented_messages + [AssistantMessage(content=history[-1][1])])))
--- src/RagPanel/components/functions/chat.py
+++ src/RagPanel/components/functions/chat.py
@@ -2,10 +2,19 @@ import gradio as gr
 
 def create_chat_tab(chat_engine):
     with gr.Blocks() as demo:
-        chat_bot = gr.Chatbot()
-        query = gr.Textbox()
-        chat_button = gr.Button("chat")
-        chat_button.click(chat_engine.get_history, None, chat_bot).then(
-            chat_engine.stream_chat, query
-        )
+        with gr.Column():
+            with gr.Row():
+                threshold_slider = gr.Slider(0, 2, value=1, step=0.02, label="threshold", info="results with Euclidean distance greater than the threshold will be filtered")
+                top_k_slider = gr.Slider(1, 32, value=5, step=1, label="top_k", info="top k document chunks with the smallest Euclidean distance will be retrieved")
+            history = chat_engine.get_history()
+            chat_bot = gr.Chatbot(label="chat", value=history)
+            with gr.Row():
+                query_box = gr.Textbox(scale=6)
+                chat_button = gr.Button("enter")
+            chat_button.click(chat_engine.get_history, None, chat_bot).then(
+                chat_engine.stream_chat, [chat_bot, query_box, threshold_slider, top_k_slider], chat_bot
+            )
+            query_box.submit(chat_engine.get_history, None, chat_bot).then(
+                chat_engine.stream_chat, [chat_bot, query_box, threshold_slider, top_k_slider], chat_bot
+            )
     return demo
