3ddacc6574250b282daac7e68d5c065038e67f93
ILSparkle <1498283309@qq.com>
2024-11-29 01:09:57 -0800

    fix

--- src/RagPanel/webui/components/create/create_database.py
+++ src/RagPanel/webui/components/create/create_database.py
@@ -3,10 +3,54 @@ import gradio as gr
 from ..info import info_clear_database, info_create_database, info_destroy_database
 
 
-locales = None
 def create_database_block(engine, LOCALES):
-    global locales
-    locales = LOCALES
+    def get_storage_path(storage):
+        default_path = ""
+        if storage is not None:
+            if storage == 'redis':
+                default_path = os.getenv("REDIS_URI", "redis://localhost:6379")
+            elif storage == 'es':
+                default_path = os.getenv("ELASTICSEARCH_URI", "http://localhost:9001")
+        else:
+            default_path = os.getenv("REDIS_URI", "redis://localhost:6379")
+        storage_path = gr.Textbox(
+            label=LOCALES["kv_storage_uri"],
+            value=default_path,
+            scale=2
+        )
+        return storage_path
+
+
+    def get_vectorstore_path(vectorstore):
+        label = LOCALES["vectorstore_uri"]
+        if vectorstore is not None:
+            if vectorstore == 'chroma':
+                default_path = os.getenv("CHROMA_PATH", "./chroma")
+                label = LOCALES["vectorstore_path"]
+            elif vectorstore == 'milvus':
+                default_path = os.getenv("MILVUS_URI", "http://localhost:19530")
+        else:
+            default_path = os.getenv("CHROMA_PATH", "./chroma")
+        vectorstore_path = gr.Textbox(
+            label=label,
+            value=default_path,
+            scale=2
+        )
+        return vectorstore_path
+
+
+    def get_vectorstore_token(vectorstore):
+        visible = False
+        if vectorstore == 'milvus':
+            visible = True
+        vectorstore_token = gr.Textbox(
+            label=LOCALES["vectorstore_token"],
+            info="ignore this if not set",
+            value="0",
+            visible=visible
+        )
+        return vectorstore_token
+
     with gr.Blocks() as demo:
         with gr.Row():
             storage = os.getenv("STORAGE", engine.supported_storages[0])
@@ -22,7 +66,7 @@ def create_database_block(engine, LOCALES):
 
             collection = gr.Textbox(
                 value="init",
-                label=locales["collection_name"],
+                label=LOCALES["collection_name"],
             )
 
         with gr.Row():
@@ -41,59 +85,11 @@ def create_database_block(engine, LOCALES):
             vectorstore_choice.change(get_vectorstore_token, vectorstore_choice, vectorstore_token)
 
         with gr.Row():
-            database_confirm_btn = gr.Button(locales["apply_and_save"])
-            database_clear_btn = gr.Button(locales["clear_database"])
+            database_confirm_btn = gr.Button(LOCALES["apply_and_save"])
+            database_clear_btn = gr.Button(LOCALES["clear_database"])
 
     database_confirm_btn.click(engine.create_database,
                                 [collection, storage_choice, storage_path, vectorstore_choice, vectorstore_path, vectorstore_token]
                                 ).success(info_create_database)
     database_clear_btn.click(engine.clear_database).success(info_clear_database)
     return demo
-
-
-def get_storage_path(storage):
-    default_path = ""
-    if storage is not None:
-        if storage == 'redis':
-            default_path = os.getenv("REDIS_URI", "redis://localhost:6379")
-        elif storage == 'es':
-            default_path = os.getenv("ELASTICSEARCH_URI", "http://localhost:9001")
-    else:
-        default_path = os.getenv("REDIS_URI", "redis://localhost:6379")
-    storage_path = gr.Textbox(
-        label=locales["kv_storage_uri"],
-        value=default_path,
-        scale=2
-    )
-    return storage_path
-
-
-def get_vectorstore_path(vectorstore):
-    label = locales["vectorstore_uri"]
-    if vectorstore is not None:
-        if vectorstore == 'chroma':
-            default_path = os.getenv("CHROMA_PATH", "./chroma")
-            label = locales["vectorstore_path"]
-        elif vectorstore == 'milvus':
-            default_path = os.getenv("MILVUS_URI", "http://localhost:19530")
-    else:
-        default_path = os.getenv("CHROMA_PATH", "./chroma")
-    vectorstore_path = gr.Textbox(
-        label=label,
-        value=default_path,
-        scale=2
-    )
-    return vectorstore_path
-
-
-def get_vectorstore_token(vectorstore):
-    visible = False
-    if vectorstore == 'milvus':
-        visible = True
-    vectorstore_token = gr.Textbox(
-        label=locales["vectorstore_token"],
-        info="ignore this if not set",
-        value="0",
-        visible=visible
-    )
-    return vectorstore_token
