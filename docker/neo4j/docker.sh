4e20100a27eb0bfa415c4ed376725ab216bbb5a2
ILSparkle <1498283309@qq.com>
2024-12-11 06:30:04 -0800

    fix neo4j docker

---
+++ docker/neo4j/docker.sh
@@ -0,0 +1,9 @@
+docker run -it --rm \
+  --publish=7474:7474 --publish=7687:7687 \
+  --env NEO4J_AUTH=none \
+  --env NEO4J_PLUGINS='["apoc","graph-data-science"]' \
+  --env NEO4J_dbms_security_procedures_unrestricted=gds.*,apoc.*\
+  --env NEO4J_dbms_security_procedures_allowlist=gds.*,apoc.*\
+  --volume=plugins:/plugins\
+  --volume=data:/data\
+  neo4j:5.11.0
